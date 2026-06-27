052249c0eb742700e937e45dde102e104bdf7434
hyos <hyozzang99@gmail.com>
2026-06-27 18:33:14 +0900

    Fix: 트랜잭션 AOP를 제거하고 synchronized 기반 동시성 제어 적용 - synchronized는 코드 블럭에만 다른 스레드의 접근을 막으므로 메서드의 끝, DB의 커밋하는 사이에 스레드가 조회 시 변경되지 않은 값이 조회되므로 직접 flush를 날려 db에 바로 반영하게 함.

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -7,17 +7,19 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
+//@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class StockService {
 
     private final StockRepository stockRepository;
 
-    @Transactional
+//    @Transactional
     public synchronized void decreaseStock(Long stockId, Long quantity) {
         Stock stock = stockRepository.findById(stockId).orElseThrow(
                 () -> new IllegalArgumentException("잘못된 상품ID입니다."));
 
         stock.decreaseStock(quantity); // 끝 dirty checking
+
+        stockRepository.saveAndFlush(stock);
     }
 }
