b7b3b846bfa0889c4d34846e78bbf12cce09a691
hyos <hyozzang99@gmail.com>
2026-06-27 19:41:29 +0900

    Feat: 비관적 락(Pessimistic Lock)을 사용한 동시성 문제 해결 - 수정 중 다른 스레드가 동시에 접근해도 기존 스레드 작업이 완료된 후에 접근 가능하므로 동시성 문제 해결

--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -1,8 +1,17 @@
 package com.example.stock.repository;
 
 import com.example.stock.domain.Stock;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select s from Stock s where s.id = :stockId")
+    Optional<Stock> findByIdWithPessimisticLock(@Param("stockId") Long stockId);
 }
---
+++ src/main/java/com/example/stock/service/PessimisticLockStockService.java
@@ -0,0 +1,20 @@
+package com.example.stock.service;
+
+import com.example.stock.domain.Stock;
+import com.example.stock.repository.StockRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class PessimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    @Transactional
+    public void decreaseStock(Long stockId, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(stockId).orElseThrow();
+        stock.decreaseStock(quantity);
+    }
+}
